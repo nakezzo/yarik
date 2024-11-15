90c5463934ebd1ac4f9de0c20bdf58c02c28d07d
nakezzo <ivanshemuda2@gmail.com>
2024-11-15 19:58:33 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = "7948582094:AAEU1z3MVaeJ9WTaucpvVKZQHWRzxCgF4Ek" # Токен бота
+CHANNEL_ID = -1002304328485 # ID канала со ставками
+ADMINS = [7024382588] # ID админов
+BET_URL = "t.me/send?start=IVUHyST8MJqb" # Ссылка на счет
+CRYPTOPAY_TOKEN = "284523:AACRIyO7eGdCaTWNB8LA5WV6qSYZE4ZsKwg" # Тут CryptoPay токен
+WIN_IMAGE = "win.mp4" # Название файла с картинкой выигрыша
+LOSE_IMAGE = "lose.mp4" # Название файла с картинкой проигрыша
+LOGS_ID = -1002469070319 # ID канала с оплатами
+
+RULES_LINK = "https://t.me/+TCxjr8zALuMxYTIy" # Ссылка на канал с правиламиW
+NEWS_LINK = "https://t.me/+Tr9fc18HIfdhOWYy" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/MARS_Sup" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "https://t.me/+FSdg8euMsjNjMWYy" # Ссылка на переходник
+BETS_LINK = "https://t.me/+2mjroqkFX6I1ZTdi" # Ссылка на канал ставок
+CASINO_NAME = "CasinoMars" # Название казино
